c65057a8e30ccc79848ebbe879bbcc168cad0dee
Rodrigo Goro <rodrigopamo323@gmail.com>
2023-02-07 22:59:45 -0500

    Casi terminado el cv

--- script.js
+++ script.js
@@ -19,3 +19,46 @@
     $menu.classList.remove("is-active");
   });
 })(document);
+
+/*Contact form */
+
+((d) => {
+  const $form = d.querySelector(".contact-form"),
+    $loader = d.querySelector(".contact-form-loader"),
+    $response = d.querySelector(".contact-form-response");
+
+  $form.addEventListener("submit", (e) => {
+    e.preventDefault(); /*evitar que se nevie el formulario*/
+    $loader.classList.remove("none"); /*para que se vea el loader*/
+    /*solicitamo el formsubmit de la pagina*/
+    fetch("https://formsubmit.co/ajax/rey_pamoide@hotmail.com", {
+      method: "POST",
+      body: new FormData(e.target),
+    })
+      /*si esta correcto regresa el res.json y si esta mal la promesa del catch*/
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        console.log(json);
+        /*como ya paso bien borramos de nuevo el loader*/
+        $loader.classList.add("none");
+        /*para que se active la ventana de agradecimiento*/
+        location.hash = "#gracias";
+        /*Limpiar el formulario*/
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message =
+          err.statusText || "Ocurrio un error al enviar, intenta nuevamente";
+        $response.querySelector(
+          "h3"
+        ).innerHTML = `Error ${err.status}: ${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add("none");
+        setTimeout(() => {
+          location.hash = "#cierre";
+        }, 3000);
+      });
+  });
+})(document);
